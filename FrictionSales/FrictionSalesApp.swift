9b470ae8e619e73b428d760c172152a731c36778
kousikbhumana <bhumanakousikreddy@gmail.com>
2026-08-14 12:47:15 +0530

    Initial Commit

---
+++ FrictionSales/FrictionSalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrictionSalesApp.swift
+//  FrictionSales
+//
+//  Created by Bhumana Lakshmi Kousik Reddy on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FrictionSalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
